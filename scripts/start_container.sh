8464ff367c783f0f83670b3f72f6a1d9a4080043
Supraja Boga <suprajaboga15@gmail.com>
2024-09-26 17:05:38 +0530

    final commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull suprajaboga/simple-python-flask-app
+docker pull suprajaboga/simple-python-flask-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 suprajaboga/simple-python-flask-app
